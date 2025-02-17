ac803a0dc8cee06e1dd225c659baabfb488711ef
akairisu <wade5071861215@gmail.com>
2025-02-17 09:42:09 +0800

    Fix stucture & scene 3

--- Assets/Scripts/MappingNormalHandler.cs
+++ Assets/Scripts/MappingNormalHandler.cs
@@ -2,19 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SawNormalHandler : MonoBehaviour
+public class MappingNormalHandler : MonoBehaviour
 {
     public Transform VirtualObject;
     public Transform DummyObject;
+    public GameObject Menu;
     public List<Renderer> RemoveList;
     public EventHandler EventHandler;
 
-    // Update is called once per frame
     public void SetLocalTransform()
     {
         VirtualObject.localPosition = DummyObject.localPosition;
         VirtualObject.localRotation = DummyObject.localRotation;
         VirtualObject.localScale = DummyObject.localScale;
         EventHandler.RemoveList = RemoveList;
+        EventHandler.Menu = Menu;
+        EventHandler.InteractionObject = VirtualObject.gameObject;
     }
 }
