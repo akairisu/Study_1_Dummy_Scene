1ed4ed723a6d91df5c24b4fdf5e209c22b6f62a1
Derek <derekdylu@gmail.com>
2025-03-11 16:06:57 +0800

    Feat: scene 2 and 3 progress system

---
+++ Assets/Scripts/PlankProgressTracker1.cs
@@ -0,0 +1,106 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlankProgressTracker1 : MonoBehaviour
+{
+    [Header("UI References")]
+    [SerializeField] private TextMeshProUGUI progressText;
+    [SerializeField] private Slider progressSlider;
+    [SerializeField] private GameObject progressDialog;
+
+    [Header("Targets")]
+    [SerializeField] private TriggerZoneSaw target1;
+    [SerializeField] private TriggerZoneSaw target2;
+    [SerializeField] private TriggerZoneSaw target3;
+    [SerializeField] private TriggerZoneSaw target4;
+
+    [Header("Progress Settings")]
+    [SerializeField] private float maxProgress = 100f;
+    [SerializeField] private float currentProgress = 0f;
+
+    private bool isProcessing = true;
+
+    private void Start()
+    {
+        // reset maxProgress to target value
+        maxProgress = target1.TargetDistance + target2.TargetDistance + target3.TargetDistance + target4.TargetDistance;
+
+        if (progressSlider != null)
+        {
+            progressSlider.maxValue = maxProgress;
+            progressSlider.value = currentProgress;
+        }
+        UpdateProgressDisplay();
+    }
+
+    private void Update()
+    {
+        if (isProcessing)
+        {
+            UpdateProgress();
+        }
+    }
+
+    public void StartProcessing()
+    {
+        isProcessing = true;
+        if (progressDialog != null)
+        {
+            progressDialog.SetActive(true);
+        }
+    }
+
+    public void StopProcessing()
+    {
+        isProcessing = false;
+    }
+
+    public void ResetProgress()
+    {
+        currentProgress = 0f;
+        if (progressSlider != null)
+        {
+            progressSlider.value = currentProgress;
+        }
+        UpdateProgressDisplay();
+    }
+
+    private void UpdateProgress()
+    {
+        if (currentProgress < maxProgress)
+        {
+            currentProgress = target1.CurrentDistance + target2.CurrentDistance + target3.CurrentDistance + target4.CurrentDistance;
+            
+            if (progressSlider != null)
+            {
+                progressSlider.value = currentProgress;
+            }
+            
+            UpdateProgressDisplay();
+        }
+        else
+        {
+            StopProcessing();
+        }
+    }
+
+    private void UpdateProgressDisplay()
+    {
+        if (progressText != null)
+        {
+            float progressPercentage = (currentProgress / maxProgress) * 100;
+            progressText.text = $"Progress: {progressPercentage.ToString("F2")}%";
+        }
+    }
+
+    public float GetProgress()
+    {
+        return currentProgress;
+    }
+
+    public bool IsProcessing()
+    {
+        return isProcessing;
+    }
+} 
